01a6be5367b3b26d10ec839f8af6073e0350d0aa
Chamika Jayarathna <failgames.lk@gmail.com>
2025-01-29 23:33:55 +0530

    create HeroSection.jsx

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import backgroundImage from '../assets/Images/background.jpeg';
+
+const HeroSection = () => {
+  return (
+    <div className='min-h-screen w-full'>
+        <div className="absolute inset-0">
+            <img src={backgroundImage} alt="backgroundImage" className='h-full w-full object-cover'/>
+            <div className="absolute inset-0 bg-gradient-to-b from-custom-teal-blue/100 to-custom-teal-blue/40"></div>
+        </div>
+        <div className="relative z-10 mx-auto mt-48 max-w-4xl text-center px-4">
+            <h2 className="text-4xl md:text-5xl font-bold text-white">Home Description Here</h2>
+            <p className="mt-4 text-lg md:text-xl text-white/90">Home Description Here Home Description Here</p>
+        </div>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-custom-teal-blue text-white">
+    <nav className="fixed top-0 left-0 right-0 z-5 text-white">
       <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16 md:h-20">
           <div className="flex items-center lg:hidden">
