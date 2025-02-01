41e8a169766711776bdff5f6f115da8125cdb24a
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-01 19:58:01 +0530

    create property page

---
+++ src/pages/PropertyPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PropertyPage = () => {
+  return (
+    <>
+        <div className="filterContainer">
+            
+        </div>
+        <div className="listContainer"></div>
+    </>
+  );
+}
+
+export default PropertyPage;
