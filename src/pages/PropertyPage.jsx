4c0508df66a50e439f5a19019a61466e7aab55ad
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-06 19:37:17 +0530

    create SidebarFilter V2

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
-import SidebarFilter from '../components/SidebarFilter';
+import React from "react";
+import NavBar from "../components/NavBar";
+import SidebarFilter from "../components/SidebarFilter";
 
 const PropertyPage = () => {
   return (
     <>
-        <NavBar />
-        <div className="h-screen flex">
-            <div className="w-1/4 p-4 border-r-4">
-                <SidebarFilter />
-            </div>
-            <div className="listContainer"></div>
+      <NavBar />
+      <div className="flex h-[calc(100vh-60px)] pt-[60px]">
+        <div className="w-1/4 h-full p-4 border-r-4 overflow-auto">
+          <SidebarFilter />
         </div>
+
+        <div className="flex-1 h-full overflow-auto p-4">
+          <div className="listContainer"></div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default PropertyPage;
