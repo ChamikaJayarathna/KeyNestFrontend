38d1d2188da3dce3ec9eebe4577a4124b14fb65a
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-11 15:15:32 +0530

    added ProprtyCard V2

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { BiSolidCarGarage } from "react-icons/bi";
 import { FaMapMarkerAlt, FaCar } from "react-icons/fa";
 import { IoMdAdd } from "react-icons/io";
 
 const PropertyCard = () => {
   return (
-    <div className="w-[305px] h-[380px] max-w-sm bg-white rounded-2xl shadow-lg overflow-hidden">
+    <div className="w-[305px] h-[380px] max-w-sm bg-gray-50 rounded-2xl shadow-lg overflow-hidden">
       <div className="relative">
         <img
           src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
@@ -13,35 +14,39 @@ const PropertyCard = () => {
         />
       </div>
 
-      <div className="">
-        <span className="inline-block bg-green-100 text-green-600 text-sm font-semibold px-3 py-1 rounded-lg">
+      <div className="flex justify-between items-center p-3">
+        <span className="inline-block bg-green-100 text-green-600 text-sm font-semibold px-3 py-1 border">
           For Sell
         </span>
 
-        <button className="w-8 h-8 bg-teal-600 text-white flex items-center justify-center rounded-full shadow-md">
+        <button className="w-8 h-8 bg-custom-teal-blue text-white flex items-center justify-center rounded-full shadow-md">
           <IoMdAdd size={20} />
         </button>
       </div>
 
-      <div className="p-4">
-        <h3 className="text-lg font-bold text-gray-900 mt-2">
-          The Property Name
-        </h3>
+      <div className="flex justify-between items-start pb-2">
+        <div>
+          <h3 className="text-lg font-bold text-gray-900">The Property Name</h3>
+          <p className="flex items-center text-gray-500 text-sm mt-1">
+            <FaMapMarkerAlt className="mr-1 text-gray-400" />
+            001, Name Road, City
+          </p>
+          <p className="text-gray-400 text-sm mt-1">#apartment #urban</p>
+        </div>
 
-        <p className="flex items-center text-gray-500 text-sm mt-1">
-          <FaMapMarkerAlt className="mr-1" /> 001, Name Road, City
-        </p>
-
-        <p className="text-gray-400 text-sm mt-1">#apartment #urban</p>
+        <div className="flex items-center text-gray-400 text-sm">
+          <BiSolidCarGarage className="text-xl mr-1" />
+          <span>03</span>
+        </div>
       </div>
 
-      {/* card footer */}
-      <div className="w-full h-[49px] px-4 bg-red-600 flex justify-between items-center">
-
-        <span className="text-lg font-bold text-gray-800">
-          LKR 102,000 <span className="text-sm text-gray-500">/Sqft</span>
+      <div className="w-full h-[49px] px-4 bg-white flex justify-between items-center">
+        <span className="text-base font-bold text-custom-teal-blue">
+          <span className="font-bold">LKR 102,000</span>
+          <span className="text-sm text-gray-500 font-normal"> /Sqft</span>
         </span>
-        <span className="text-lg font-bold text-gray-800">Apartment</span>
+
+        <span className="text-lg font-bold text-black">Apartment</span>
       </div>
     </div>
   );
