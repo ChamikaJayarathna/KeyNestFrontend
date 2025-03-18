d7e1c000896db0e41fce5b038c231107c0791086
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-18 13:27:33 +0530

    update ProfileDropdown.jax v2

--- src/components/ProfileDropdown.jsx
+++ src/components/ProfileDropdown.jsx
@@ -3,17 +3,14 @@ import { Link } from "react-router-dom";
 
 const ProfileDropdown = ({ setShowDropdown }) => {
   return (
-    <div className="absolute right-0 mt-2 w-48 bg-white text-black shadow-lg rounded-lg overflow-hidden">
-      <Link
-        to="#"
-        className="block px-4 py-2 hover:bg-gray-100"
-      >
+    <div className="absolute right-0 mt-1 w-48 bg-white text-black shadow-lg rounded-lg overflow-hidden">
+      <Link to="#" className="block px-6 py-5 hover:bg-gray-100">
         Profile
       </Link>
-      
-      <button
-        className="block w-full text-left px-4 py-2 hover:bg-gray-100"
-      >
+
+      <hr className="border-t border-gray-300 mx-auto w-[92%]" />
+
+      <button className="block w-full text-left px-6 py-5 hover:bg-gray-100">
         Logout
       </button>
     </div>
