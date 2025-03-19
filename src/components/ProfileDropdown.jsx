1967cf8b2ad5909b86fbfcd83180b8497072c018
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-19 16:26:20 +0530

    ProfileDropdown update

--- src/components/ProfileDropdown.jsx
+++ src/components/ProfileDropdown.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProfileDropdown = ({ setShowDropdown }) => {
+const ProfileDropdown = () => {
   return (
     <div className="absolute right-0 mt-1 w-48 bg-white text-black shadow-lg rounded-lg overflow-hidden">
+      <Link to="/admin/dashboard" className="block px-6 py-5 hover:bg-gray-100">
+        Dashboard
+      </Link>
+
+      <hr className="border-t border-gray-300 mx-auto w-[92%]" />
+
       <Link to="#" className="block px-6 py-5 hover:bg-gray-100">
         Profile
       </Link>
