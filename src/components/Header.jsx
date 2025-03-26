77d81fcf2fcb5153de6969d9b3aa1d976405f241
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-26 12:36:58 +0530

    update Header.jsx v1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import { Button } from './ui/button';
+import React from "react";
+import { Button } from "./ui/button";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <section className='flex justify-between items-center shadow-sm p-4 relative'>
-      <div className="flex items-center gap-2">
+    <section className="flex justify-between items-center shadow-sm p-4 relative">
+      <div className="flex items-center gap-2 ml-5">
         <img src="./logo.svg" width={40} height={40} />
       </div>
-      <ul className='hidden md:flex gap-16 ml-25'>
-        <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
-        <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Property</li>
-        <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Services</li>
-        <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Contact</li>
+      <ul className="hidden md:flex gap-16 ml-25">
+        <NavLink to={"/"}>
+          <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+            Home
+          </li>
+        </NavLink>
+        <NavLink to={"/property"}>
+          <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+            Property
+          </li>
+        </NavLink>
+        <NavLink>
+          <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+            Services
+          </li>
+        </NavLink>
+        <NavLink>
+          <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+            Contact
+          </li>
+        </NavLink>
       </ul>
-      <div className="flex gap-4">
-        <Button>Sign in</Button>
-        <Button>Join</Button>
+      <div className="flex mr-5">
+        <Link to={"/sign-in"}>
+          <Button>Join</Button>
+        </Link>
       </div>
     </section>
   );
-}
+};
 
 export default Header;
