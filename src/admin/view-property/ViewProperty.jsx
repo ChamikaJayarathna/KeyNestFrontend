01f0dcc69a80e4afe3e960cfc334a3333690e5f1
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-29 00:29:34 +0530

    create PropertyItem.jsx

--- src/admin/view-property/ViewProperty.jsx
+++ src/admin/view-property/ViewProperty.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../admin-components/Header';
 
 const ViewProperty = () => {
   return (
-    <div>ViewProperty</div>
+    <div>
+      <Header title={"View Property"} />
+      <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-7 gap-8">
+
+      </div>
+    </div>
   );
 }
 
