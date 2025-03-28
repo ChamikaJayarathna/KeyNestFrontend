9719c14a99393eb5bf85e7c6c74fa0e4d12acd04
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-28 19:34:44 +0530

    update UploadImages.jsx v2

--- src/admin/add-property/components/UploadImages.jsx
+++ src/admin/add-property/components/UploadImages.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const UploadImages = () => {
+  const [selectedFileList, setSelectedFileList] = useState([]);
+
+  const onFileSelected = (event) => {
+    const files = event.target.files;
+    const fileArray = Array.from(files);
+    setSelectedFileList((prev) => [...prev, ...fileArray]);
+  };
+
   return (
     <div>
-        <h2 className="font-medium text-xl my-3">Upload Property Images</h2>
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
-            <label htmlFor="upload-images">
-                <div className="border rounded-xl border-dotted border-primary bg-blue-100 p-10 cursor-pointer hover:shadow-md">
-                    <h2 className="text-lg text-center text-primary">+</h2>
-                </div>
-            </label>
-            <input type="file" multiple id="upload-images" className="hidden" accept="image/*" />
-        </div>
+      <h2 className="font-medium text-xl my-3">Upload Property Images</h2>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
+        {selectedFileList.map((file, index) => (
+          <div key={index} className="relative">
+            <IoMdCloseCircle className="absolute m-2 text-lg text-white cursor-pointer" />
+            <img
+              src={URL.createObjectURL(file)}
+              className="w-full h-[130px] object-cover rounded-xl"
+            />
+          </div>
+        ))}
+        <label htmlFor="upload-images">
+          <div className="border rounded-xl border-dotted border-primary bg-blue-100 p-10 cursor-pointer hover:shadow-md">
+            <h2 className="text-lg text-center text-primary">+</h2>
+          </div>
+        </label>
+        <input
+          type="file"
+          multiple
+          id="upload-images"
+          className="hidden"
+          onChange={onFileSelected}
+          accept="image/*"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default UploadImages;
