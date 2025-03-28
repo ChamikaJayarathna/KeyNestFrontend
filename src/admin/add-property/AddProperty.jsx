d2dc0ef7b4f1a16c19f0f52d4ca36ac41b95016d
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-28 12:09:21 +0530

    update AddProperty

--- src/admin/add-property/AddProperty.jsx
+++ src/admin/add-property/AddProperty.jsx
@@ -21,16 +21,19 @@ const AddProperty = () => {
 
   return (
     <div>
-      <Header title={"Add Property"}/>
+      <Header title={"Add Property"} />
       <div className="px-10 md:px-20 my-10">
         <h2 className="font-bold text-4xl">Add New Property</h2>
         <form className="p-10 border rounded-xl mt-10" onSubmit={handleSubmit}>
           {/* Property Details */}
           <div>
             <h2 className="font-medium text-xl mb-6">Property Details</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="grid grid-cols-2 gap-5">
               {propertyDetails.propertyDetails.map((item, index) => (
-                <div key={index}>
+                <div
+                  key={index}
+                  className={item.fieldType === "textarea" ? "col-span-2" : ""}
+                >
                   <label className="text-sm flex gap-2 items-center mb-2">
                     <IconField icon={item?.icon} />
                     {item?.label}{" "}
@@ -47,10 +50,12 @@ const AddProperty = () => {
                       handleInputChange={handleInputChange}
                     />
                   ) : item.fieldType === "textarea" ? (
-                    <TextAreaField
-                      item={item}
-                      handleInputChange={handleInputChange}
-                    />
+                    <div className="grid grid-cols-1">
+                      <TextAreaField
+                        item={item}
+                        handleInputChange={handleInputChange}
+                      />
+                    </div>
                   ) : null}
                 </div>
               ))}
