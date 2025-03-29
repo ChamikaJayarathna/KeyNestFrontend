82628f9b4f1cba49081428a7dc4e61417457d8a5
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-29 08:55:35 +0530

    update PropertyItem.jsx v2

--- src/admin/view-property/components/PropertyItem.jsx
+++ src/admin/view-property/components/PropertyItem.jsx
@@ -4,9 +4,9 @@ import { Separator } from "@/components/ui/separator";
 const PropertyItem = ({ property }) => {
   return (
     <div className="rounded-xl bg-white border hover:shadow-md cursor-pointer">
-        <h2 className="absolute m-2 bg-green-500 px-2 rounded-full text-sm text-white">
-            {property?.type}
-        </h2>
+      <h2 className="absolute m-2 bg-green-500 px-2 rounded-full text-sm text-white">
+        {property?.type}
+      </h2>
       <img
         src={property?.images[0]}
         width={"100%"}
@@ -16,7 +16,10 @@ const PropertyItem = ({ property }) => {
 
       <div className="p-4">
         <h2 className="font-bold text-black text-lg mb-2">{property?.title}</h2>
-        <Separator />
+        <Separator className="my-2" />
+        <div className="flex items-center justify-between text-black">
+          <h2 className="font-bold text-xl">LKR {property?.price}</h2>
+        </div>
       </div>
     </div>
   );
