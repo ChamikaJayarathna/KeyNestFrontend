c94fc7397414b650cbde21925a7e280f276a0312
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-01 09:26:08 +0530

    update PropertyDetailsCard.jsx v1

--- src/components/property-details/components/Map.jsx
+++ src/components/property-details/components/Map.jsx
@@ -10,7 +10,7 @@ const Map = ({ propertyDetails }) => {
         center={[propertyDetails?.latitude, propertyDetails?.longitude]}
         zoom={13}
         scrollWheelZoom={false}
-        style={{ height: "400px", width: "100%" }}
+        style={{ height: "300px", width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
