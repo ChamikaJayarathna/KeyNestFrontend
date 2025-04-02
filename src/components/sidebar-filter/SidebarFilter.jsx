ac47a3af3a56729fb63739765931c3a2b5695b56
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-02 12:45:55 +0530

    update SidebarFilter.jsx

--- src/components/sidebar-filter/SidebarFilter.jsx
+++ src/components/sidebar-filter/SidebarFilter.jsx
@@ -8,9 +8,9 @@ import AccessibilityFeatures from "./components/AccessibilityFeatures";
 import Specification from "./components/Specification";
 import Condition from "./components/Condition";
 import SaleMethod from "./components/SaleMethod";
+import { Button } from "../ui/button";
 
 const SidebarFilter = () => {
-
   const [filterData, setFilterData] = useState({});
 
   const handleFilterChange = (name, isChecked) => {
@@ -22,41 +22,52 @@ const SidebarFilter = () => {
 
   return (
     <div className="p-6 space-y-6 border-r-3 h-screen overflow-y-auto sticky top-0 no-scrollbar">
+      <PropertyTypes
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
 
-      <PropertyTypes filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
-      <Specification/>
+      <Specification />
 
-      {/* <div>
-        <h2 className="text-custom-teal-blue text-base font-bold mb-2">
-          Land size
-        </h2>
-        <div className="flex gap-4">
-          <div className="w-1/2">
-            <div className="text-sm text-gray-600 mb-1">Min</div>
-            <select className="w-full p-2 border rounded">
-              <option>Any</option>
-            </select>
-          </div>
-          <div className="w-1/2">
-            <div className="text-sm text-gray-600 mb-1">Max</div>
-            <select className="w-full p-2 border rounded">
-              <option>Any</option>
-            </select>
-          </div>
-        </div>
-      </div> */}
+      <Condition
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
 
-      <Condition filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
-      <SaleMethod filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
-      <OutdoorFeatures filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
-      <IndoorFeatures filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
-      <ClimateFeatures filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
-      <AccessibilityFeatures filter={filter} handleFilterChange={handleFilterChange} filterData={filterData}/>
+      <SaleMethod
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
+
+      <OutdoorFeatures
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
+
+      <IndoorFeatures
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
+
+      <ClimateFeatures
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
+
+      <AccessibilityFeatures
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+        filterData={filterData}
+      />
 
       <div className="flex justify-end">
-        <button className="bg-black text-white py-2 px-6 rounded text-lg">
-          Apply
-        </button>
+        <Button>Apply</Button>
       </div>
     </div>
   );
