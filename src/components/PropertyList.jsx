91750986475ad1c1e98f1d31fa1c5dc56b77267c
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-02 08:17:50 +0530

    update PropertyList.jsx

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import PropertyCard from "./PropertyCard";
 import apiRequest from "../lib/apiRequest";
+import SearchBox from "./SearchBox";
 
 const PropertyList = () => {
-
   const [propertyDetails, setPropertyDetails] = useState([]);
 
   useEffect(() => {
@@ -12,7 +12,7 @@ const PropertyList = () => {
 
   const GetAllProperty = async () => {
     try {
-      const response = await apiRequest.get('/property/get-all-property');
+      const response = await apiRequest.get("/property/get-all-property");
       setPropertyDetails(response.data);
     } catch (error) {
       console.log(error);
@@ -21,31 +21,13 @@ const PropertyList = () => {
 
   return (
     <div>
-      <div className="flex justify-end mr-25">
-        <div
-          className="flex items-center border border-custom-teal-blue rounded-lg overflow-hidden max-w-md"
-          style={{ width: "426px", height: "50px" }}
-        >
-          <input
-            type="text"
-            placeholder="Search"
-            className="w-full p-2 outline-none"
-          />
-          <button
-            className="bg-black text-white text-base px-6 py-2 font-semibold"
-            style={{ height: "100%" }}
-          >
-            Search
-          </button>
-        </div>
-      </div>
+      <SearchBox />
 
       <div className="grid grid-cols-4 gap-32">
         {propertyDetails.map((property) => (
           <PropertyCard key={property.id} property={property} />
         ))}
       </div>
-
     </div>
   );
 };
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const SearchBox = () => {
   return (
-    <div>SearchBox</div>
+    <div>
+      <div className="flex justify-end">
+        <div
+          className="flex items-center border border-custom-teal-blue rounded-lg overflow-hidden max-w-md"
+          style={{ width: "426px", height: "50px" }}
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full p-2 outline-none"
+          />
+          <button
+            className="bg-black text-white text-base px-6 py-2 font-semibold cursor-pointer"
+            style={{ height: "100%" }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default SearchBox;
