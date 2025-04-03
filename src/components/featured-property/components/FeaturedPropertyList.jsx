b50ec3fd91eda6dddab349ab4baa13d82d119b99
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-03 22:27:45 +0530

    update FeaturedPropertyList.jsx

--- src/components/featured-property/components/FeaturedPropertyList.jsx
+++ src/components/featured-property/components/FeaturedPropertyList.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel"
 
 const FeaturedPropertyList = () => {
   return (
-    <div>FeaturedPropertyList</div>
+    <div className='mx-24 hidden md:block'>
+        {/* <h2 className='font-bold text-3xl text-center mt-16 mb-7'>Most Searched Cars</h2> */}
+        <Carousel>
+            <CarouselContent>
+                {/* {carListings.map((car, index) => (
+                    <CarouselItem className="basis-1/2 md:basis-1/3 lg:basis-1/4">
+                        <CarItem car={car} />
+                    </CarouselItem>
+                ))} */}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    </div>
   );
 }
 
