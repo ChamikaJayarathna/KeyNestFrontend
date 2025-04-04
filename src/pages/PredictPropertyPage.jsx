a5d6767e0c73e7036702eb5d9917e926b309170d
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-04 20:37:37 +0530

    update PredictPropertyPage.jsx

--- src/pages/PredictPropertyPage.jsx
+++ src/pages/PredictPropertyPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropertyCard from "../components/PropertyCard";
 import { useLocation } from "react-router-dom";
+import Header from "@/components/Header";
 
 const PredictPropertyPage = () => {
   const location = useLocation();
@@ -8,9 +9,10 @@ const PredictPropertyPage = () => {
 
   return (
     <div>
+      <Header/>
       <div className="p-10 md:px-20">
         <h2 className="font-bold text-4xl">Predicted Result</h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-7">
+        <div className="px-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-7">
           {prediction.length > 0
             ? prediction.map((property, index) => (
                 <PropertyCard key={index} property={property} />
