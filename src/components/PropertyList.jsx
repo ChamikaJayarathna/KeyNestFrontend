360e93b372069529fa0a14d877cfa5f4a2428c47
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-04 18:31:00 +0530

    update PropertyList.jsx

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -4,16 +4,18 @@ import SearchBox from "./SearchBox";
 
 const PropertyList = ({ propertyDetails }) => {
   return (
-    <div>
+    <div className="px-25 py-4">
       <SearchBox />
 
-      <div className="px-8 grid grid-cols-1 md:grid-cols-4 gap-45">
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {propertyDetails.length > 0 ? (
           propertyDetails.map((property) => (
             <PropertyCard key={property.id} property={property} />
           ))
         ) : (
-          <p>No properties found</p>
+          <p className="col-span-full text-center text-gray-500">
+            No properties found
+          </p>
         )}
       </div>
     </div>
