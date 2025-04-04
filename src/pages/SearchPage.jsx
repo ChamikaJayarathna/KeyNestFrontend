72f0c2a8e7ce1581872d2cec3ec84ebd7f5c18b2
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-04 21:23:38 +0530

    update SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -6,10 +6,18 @@ import Header from "@/components/Header";
 
 const SearchPage = () => {
   const [property, setProperty] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { query } = useParams();
 
   useEffect(() => {
+    setLoading(true);
     SearchProperty();
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 10000);
+
+    return () => clearTimeout(timer);
   }, [query]);
 
   const SearchProperty = async () => {
@@ -25,21 +33,28 @@ const SearchPage = () => {
 
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="p-15 md:px-20">
-        <div className="grid grid-cols-4 gap-8">
-          {property.length > 0
-            ? property.map((item, index) => (
-                <div key={index}>
-                  <PropertyCard property={item} />
-                </div>
-              ))
-            : [1, 2, 3, 4, 5, 6].map((item, index) => (
-                <div
-                  key={index}
-                  className="h-[320px] rounded-xl bg-slate-200 animate-pulse"
-                ></div>
-              ))}
+        <h2 className="font-bold text-4xl">Search Result</h2>
+        <div className="px-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
+          {property.length > 0 ? (
+            property.map((item, index) => (
+              <div key={index}>
+                <PropertyCard property={item} />
+              </div>
+            ))
+          ) : loading ? (
+            [...Array(6)].map((_, index) => (
+              <div
+                key={index}
+                className="h-[320px] rounded-xl bg-slate-200 animate-pulse"
+              ></div>
+            ))
+          ) : (
+            <div className="col-span-full text-center text-gray-500 text-lg">
+              No search result found.
+            </div>
+          )}
         </div>
       </div>
     </div>
