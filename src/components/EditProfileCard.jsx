b31a75eb968ec9a6ffba6ecb9e859fe73cf6bb89
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-06 19:40:13 +0530

    update ProfileCard.jsx v1

--- src/components/EditProfileCard.jsx
+++ src/components/EditProfileCard.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const EditProfileCard = () => {
-  return (
-    <div>EditProfileCard</div>
-  );
-}
+const EditProfileCard = ({ profileDetail }) => {
+  return <div>EditProfileCard</div>;
+};
 
 export default EditProfileCard;
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -4,6 +4,7 @@ import { jwtDecode } from "jwt-decode";
 import apiRequest from "@/lib/apiRequest";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
+import EditProfileCard from "./EditProfileCard";
 
 const ProfileCard = () => {
   const [profileDetail, setProfileDetail] = useState();
@@ -33,22 +34,36 @@ const ProfileCard = () => {
 
   return (
     <div className="w-[500px] h-[450px] mx-auto bg-amber-300 p-8 rounded-lg shadow-lg mt-10 ">
-      <img
-        src={profileDetail?.profile_img}
-        className="w-30 h-30 mx-auto mb-4 object-cover"
-      />
-      <div className="text-center mt-5">
-        <h1 className="text-3xl font-bold text-gray-800">
-          {profileDetail?.username}
-        </h1>
-        <p className="text-gray-600 text-base">{profileDetail?.email}</p>
-      </div>
-      <div className="flex justify-center gap-4 mt-20">
-        <Button className="flex-1 max-w-[180px]">Edit Profile</Button>
-        <Button className="flex-1 max-w-[180px]" onClick={handleLogout}>
-          Logout
-        </Button>
-      </div>
+      {!isEditing ? (
+        <>
+          <img
+            src={profileDetail?.profile_img}
+            className="w-30 h-30 mx-auto mb-4 object-cover"
+          />
+          <div className="text-center mt-5">
+            <h1 className="text-3xl font-bold text-gray-800">
+              {profileDetail?.username}
+            </h1>
+            <p className="text-gray-600 text-base">{profileDetail?.email}</p>
+          </div>
+          <div className="flex justify-center gap-4 mt-20">
+            <Button
+              className="flex-1 max-w-[180px]"
+              onClick={() => setIsEditing(true)}
+            >
+              Edit Profile
+            </Button>
+            <Button className="flex-1 max-w-[180px]" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        </>
+      ) : (
+        <EditProfileCard
+          profileDetail={profileDetail}
+          setIsEditing={setIsEditing}
+        />
+      )}
     </div>
   );
 };
