828c078ca9d8ab2c1f34d3f18c6ad2b5b7fd5987
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-06 10:57:12 +0530

    update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import Header from "@/components/Header";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React from "react";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+      <Header />
+      <div className="px-10 md:px-20 my-10">
+        <Tabs defaultValue="profile" className="w-full">
+          <TabsList className="w-full flex justify-start">
+            <TabsTrigger value="profile">Profile</TabsTrigger>
+            <TabsTrigger value="chat">Chat</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="profile">Profile</TabsContent>
+          <TabsContent value="chat">Chat</TabsContent>
+        </Tabs>
+      </div>
+    </div>
   );
-}
+};
 
 export default Profile;
