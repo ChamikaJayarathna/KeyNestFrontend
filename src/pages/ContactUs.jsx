b0a6a0ec79a44a9178557b80abdce4cf1c4293bf
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-06 22:26:32 +0530

    create ContactUs.jsx v1

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,8 +1,11 @@
+import Header from '@/components/Header';
 import React from 'react';
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+        <Header/>
+    </div>
   );
 }
 
