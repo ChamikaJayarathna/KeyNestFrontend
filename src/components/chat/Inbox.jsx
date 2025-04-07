1e5fe83bf159e32a7434555d766ea687a00a249a
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-07 17:31:36 +0530

    update ChatSidebar.jsx

--- src/components/chat/Inbox.jsx
+++ src/components/chat/Inbox.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import ChatSidebar from "./components/ChatSidebar";
 
 const Inbox = () => {
   return (
-    <div>Inbox</div>
+    <div className="flex h-screen bg-gray-100 font-sans mt-10">
+      <ChatSidebar />
+    </div>
   );
-}
+};
 
 export default Inbox;
