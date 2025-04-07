255c1fb4645ed7709d4f3865706a53be36f9c1af
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-07 07:50:48 +0530

    update MostSearchedProperty.jsx

--- src/components/MostSearchedProperty.jsx
+++ src/components/MostSearchedProperty.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import apiRequest from "@/lib/apiRequest";
+import React, { useEffect, useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import PropertyCard from "./PropertyCard";
 
 const MostSearchedProperty = () => {
+  const [propertyDetails, setPropertyDetails] = useState([]);
+
+  const GetAllProperty = async () => {
+    try {
+      const response = await apiRequest.get("/property/get-all-property");
+      setPropertyDetails(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    GetAllProperty();
+  }, []);
+
   return (
-    <div>MostSearchedProperty</div>
+    <div className="mx-24 hidden md:block">
+      <h2 className="font-bold text-3xl text-center mt-16 mb-7">
+        Most Searched Property
+      </h2>
+      <Carousel>
+        <CarouselContent>
+          {propertyDetails.map((property, index) => (
+            <CarouselItem className="basis-1/2 md:basis-1/3 lg:basis-1/4">
+                <PropertyCard property={property} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
-}
+};
 
 export default MostSearchedProperty;
