d70f89f48b8adeadbb89fd99147d5dafc2cebf7e
Chamika Jayarathna <failgames.lk@gmail.com>
2025-04-07 07:53:13 +0530

    update MostSearchedProperty.jsx v1

--- src/components/MostSearchedProperty.jsx
+++ src/components/MostSearchedProperty.jsx
@@ -34,7 +34,7 @@ const MostSearchedProperty = () => {
         <CarouselContent>
           {propertyDetails.map((property, index) => (
             <CarouselItem className="basis-1/2 md:basis-1/3 lg:basis-1/4">
-                <PropertyCard property={property} />
+              <PropertyCard property={property} />
             </CarouselItem>
           ))}
         </CarouselContent>
